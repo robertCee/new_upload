4566c284d7f5a643e166ac86cd1a7a4ab36bfc21
robertCee <119926459+robertCee@users.noreply.github.com>
2022-12-07 08:52:22 +0800

    Update StudentDataController.php

--- app/Http/Controllers/StudentDataController.php
+++ app/Http/Controllers/StudentDataController.php
@@ -9,13 +9,10 @@ class StudentDataController extends Controller
     public function index() {
         $students = Student::paginate(20);
 
-        // yung variable $students need i compact papuntang view para magamit mo sa @foreach yung collection niya
-        return view('students.index', compact('students'));
+         return view('students.index', compact('students'));
     }
 
-    // Etong function name is yung naka assign dun sa route after ng url
-    // Yung $id parameter is yung id na nasa url sa route mo sa web.php
-    public function deleteStudent($id) {
+     public function deleteStudent($id) {
         $deleteThis = StudentData::find($id)->delete();
         return redirect()->back();
     }
